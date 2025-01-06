ad8a96ca1fb40d4b4e37459e2e1bfd4d8d3f0ead
Aditya Kirti <addyyy118@gmail.com>
2025-01-06 12:04:43 +0530

    Fix folder col width

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -2,24 +2,7 @@ const multer = require("multer");
 const path = require("path");
 const File = require("../prisma/queries/File");
 
-const diskStorage = multer.diskStorage({
-  destination: (req, file, done) => {
-    done(null, path.join(__dirname, "../uploads"));
-  },
-  filename: (req, file, done) => {
-    const fileExt = path.extname(file.originalname);
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    done(null, file.fieldname + "-" + uniqueSuffix + fileExt);
-  },
-});
 
-const uploadFiles = multer({
-  storage: diskStorage,
-  limits: {
-    fileSize: 50 * 1000 * 1000,
-    files: 10,
-  },
-}).array("files", 10);
 
 exports.getUpload = (req, res) => {
   const { folderId } = req.params;
